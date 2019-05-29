27da16fd607a753d34f751feac1b920c210204c0
colin <clawrence005@yahoo.com>
2019-05-29 14:25:24 -0500

    adding colins backend code

---
+++ config/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'secret': 'passphrase',
+  // 'database': 'mongodb://localhost:27017/jwtpassport'
+  'database': 'mongodb://localhost:27017/tacolist'
+};
+// module.exports = {
+//   mongoURI: "YOUR_MONGOURI_HERE",
+//   secretOrKey: "passphrase"
+// };
---
+++ config/passport.js
@@ -0,0 +1,29 @@
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+// var jwt = require('jsonwebtoken');
+// var mongoose = require('mongoose');
+var User = require('../models/user');
+// const User = mongoose.model("users");
+var config = require('../config/.env');
+var passport = require('passport');
+// require('../config/passport')(passport);
+
+// Setup work and export for the JWT passport strategy
+module.exports = function (passport) {
+  var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+  opts.secretOrKey = config.secret;
+  // console.log("passport.js", passport.use, "END\n~~~~~~");
+  passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.id }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    });
+  }));
+};
